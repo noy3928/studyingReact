ca8adb10949b8600a21e0ac00d3fd7c74c92ec0d
noy3928@naver.com <noy3928@naver.com>
2021-07-14 00:42:35 +0900

    useState와 리액트 훅의 원칙

---
+++ react-hooks/hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+
+import Card from '../UI/Card';
+import './IngredientForm.css';
+
+const IngredientForm = React.memo(props => {
+  //useState는 내부에 2개의 배열을 가진다. 첫번째 배열은 이전상태, 두번째 배열은 이후의 상태이다. 
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
+  //리액트 훅의 중요한 규칙 1) 함수형에서만 사용 가능하다. 2) 컴포넌트의 root에서 선언해야한다. nested된 형식으로 선언해서는 안된다. if문에서든 함수에서든, 어쩃뜬 안에서 선언되어서는 안된다. 
+
+  const submitHandler = event => {
+    event.preventDefault();
+    // ...
+  };
+
+  return (
+    <section className="ingredient-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input type="text" id="title" value={enteredTitle} onChange={event=> {
+            setEnteredTitle(event.target.value)}} />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input type="number" id="amount" value={enteredAmount} onChange={event=> {
+            setEnteredAmount(event.target.value);
+            }}  />
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default IngredientForm;
---
+++ redux-udemy/redux-demo.js
@@ -0,0 +1,29 @@
+const redux = require('redux');
+
+//우리는 먼저 스토어를 생성하고, 그 다음 리듀서를 생성해야한다. 그런 다음 컴포넌트를 만들고, 액션을 만든다. 
+
+//리듀서는 항상 두가지 인자를 받는다. 1)이전 state와 2) dispatch Acion이다. 리듀서는 항상 새로운 값을 내본ㄹ 것이다. 주의해야할 점이 있다면, 그 값의 형태는 항상 변해서는 안된다. 항상 기대 가는한 값을 배출해주는 것이 리듀서이다. 
+const counterReducer = (state ={counter:0}, action) => {
+    //state는 어떤 값이든 될 수 있다. 그렇지만 통상적으로는 객체로 사용된다. 
+    return{
+        counter: state.counter + 1
+    };
+};
+//1.스토어를 생성한다 우리는 이 스토어를 가지고 무엇을 할 것이니가? 데이터를 관리할 것이다. 그런데 그 데이터는 리듀서에 의해서 결정된다. 리듀서는 새로운 데이터를 만들어낸다. 그리고 스토어에 주입한다. 
+const store = redux.createStore(counterReducer);//스토어는 어떤 리듀서가 데이터에 영향을 미치는지 알 필요가 있다. 그래서 파라미터에 리듀서를 넣어준다. 
+
+//리듀서와 스토어 설정이 끝났다면 우리는 이제 스토어를 구독할 누군가가 필요하다. 
+
+console.log(store.getState())
+
+
+const counterSubscriber = () => {
+    //getState()는 항상 가장 최신의 데이터를 업데이트 해준다. 
+    const latestState = store.getState()
+    console.log(latestState);
+}
+
+//subscribe는 구독함수를 필요로 한다. 그리고 그 함수에 대해서, 스토어의 데이터를 기대한다. 
+store.subscribe(counterSubscriber);
+
+store.dispatch({type: "increment" });
