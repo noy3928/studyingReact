87e4377638b08465b4390aca04e2e6ca8e8f7335
noy3928@naver.com <noy3928@naver.com>
2021-07-14 23:56:28 +0900

    리듀서 활용하기 리듀서에 함수추가하기

--- redux-udemy/01-starting-project/src/components/Counter.js
+++ redux-udemy/01-starting-project/src/components/Counter.js
@@ -2,73 +2,80 @@ import { useSelector, useDispatch, connect } from "react-redux";
 import classes from "./Counter.module.css";
 import { Component } from "react";
 
-// const Counter = () => {
-//   const dispatch = useDispatch();
-//   const counter = useSelector((state) => state.counter);
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
 
-//   const incrementHandler = () => {
-//     dispatch({ type: "increment" });
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
+  const decrementHandler = () => {
+    dispatch({
+      type: "decrement",
+    });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
+
+// //이번에는 위에있는 함수형 컴포넌트를 클래스형 컴포넌트로 바꾸는 연습을 해보겠다.
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this.)}>Increment</button>
+//           <button onClick={this.decrementHandler.bint(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
 //   };
-
-//   const decrementHandler = () => {
-//     dispatch({
-//       type: "decrement",
-//     });
-//   };
-
-//   const toggleCounterHandler = () => {};
-
-//   return (
-//     <main className={classes.counter}>
-//       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{counter}</div>
-//       <div>
-//         <button onClick={incrementHandler}>Increment</button>
-//         <button onClick={decrementHandler}>Decrement</button>
-//       </div>
-//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-//     </main>
-//   );
 // };
 
-//이번에는 위에있는 함수형 컴포넌트를 클래스형 컴포넌트로 바꾸는 연습을 해보겠다.
-class Counter extends Component {
-  incrementHandler() {
-    this.props.increment();
-  }
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
 
-  decrementHandler() {
-    this.props.decrement();
-  }
-
-  toggleCounterHandler() {}
-
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-          <button onClick={this.decrementHandler.bint(this)}>Decrement</button>
-        </div>
-        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    );
-  }
-}
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: () => dispatch({ type: "increment" }),
-    decrement: () => dispatch({ type: "decrement" }),
-  };
-};
-
-//connect함수는 클래스형에서만 사용한다. 그리고 이것은 하이오더 함수이다.
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+// //connect함수는 클래스형에서만 사용한다. 그리고 이것은 하이오더 함수이다.
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- redux-udemy/01-starting-project/src/store/index.js
+++ redux-udemy/01-starting-project/src/store/index.js
@@ -1,22 +1,27 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if(action.type === 'increment'){
-        return{
-            counter: state.counter + 1
-        }
-    }
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
 
-    if(action.type === "decrement"){
-        return{
-            counter: state.counter - 1,
-        }
-    }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+    };
+  }
 
-    return state;
-}
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
 
 const store = createStore(counterReducer);
 
 export default store;
-
