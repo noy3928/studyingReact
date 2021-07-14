b47c9a34020430dee7939115b15dea65ae631b3c
noy3928@naver.com <noy3928@naver.com>
2021-07-15 00:04:15 +0900

    이니셜 스테이트를 만들고, boolean값을 만들어서 상태에따라 보여주는 요소만들기

--- redux-udemy/01-starting-project/src/components/Counter.js
+++ redux-udemy/01-starting-project/src/components/Counter.js
@@ -5,6 +5,7 @@ import { Component } from "react";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -20,12 +21,16 @@ const Counter = () => {
     });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({
+      type: "toggle",
+    });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment by 5</button>
