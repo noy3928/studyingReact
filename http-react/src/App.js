98eddf000590f29bc0e6c17ed618b997fadf43f4
노예찬 <noyechan@noyechan-ui-MacBookPro.local>
2021-07-25 16:21:51 +0900

    async with fetch api

---
+++ http-react/src/App.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([])
+
+  async function fetchMoviesHandler() {
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json();
+
+    const transformdMovies = data.results.map(movieData => {
+        return{
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformdMovies)
+
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
