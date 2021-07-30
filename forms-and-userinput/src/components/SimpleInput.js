7d0a8cd22515ea9047befa9f10632532bbf8be23
yechan <noy3928@naver.com>
2021-07-31 03:31:31 +0900

    인풋의 조건부를 확인하기

--- forms-and-userinput/src/components/SimpleInput.js
+++ forms-and-userinput/src/components/SimpleInput.js
@@ -1,9 +1,16 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if(enteredNameIsValid){
+      console.log('Name Input is valid!')
+    }
+   } ,[enteredNameIsValid])
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -12,6 +19,8 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = event => {
     event.preventDefault(); // 브라우저 상에서 기본적으로 일어나는 행위를 막는다. 그 행위로써는 섭밋이 될 때, http request가 보내지는 것이 될 수 있겠다. 
   
+    setEnteredNameTouched(true);
+
     if(enteredName.trim() === ''){
       setEnteredNameIsValid(false);
       return;
@@ -26,13 +35,15 @@ const SimpleInput = (props) => {
     // nameInputRef.current.value = '' // 이런 방법도 있지만, 그렇게 좋은 방법은 아니다. 
     setEnteredName('')//이렇게 하면 제출이 된 후 폼의 내용물이 지워질 수 있다. 
   }
+  
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
 
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='nameInputClasses'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input 
           ref={nameInputRef} 
@@ -41,7 +52,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler} 
           value={enteredName}
           />
-          {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
+          {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
