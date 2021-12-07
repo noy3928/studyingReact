5399fb7aacce4cb5ecf0d8124863ae250c3fcef1
노예찬 <noy3928@cclss.net>
2021-12-07 22:45:38 +0900

    기본적인 post 사가 만들기

---
+++ redux-saga-tutorial-youtube-started-basic-tutorial/src/modules/posts/Posts.saga.js
@@ -0,0 +1,17 @@
+import {takeEvery, all, call, put} from 'redux-saga/effects'
+import {GET_POSTS} from "./Posts.actions"
+import {getPosts} from "../../utils/api"
+
+//액션을 디스패치 하기 위해서는 put을 사용하고, 함수를 호출하기 위해서는 call을 사용한다. 
+
+function* getPostsSaga(){
+    getPosts()
+}
+
+function* getPostsWatcher() {
+    yield takeEvery(GET_POSTS, getPostsSaga) // takeEvery의 첫번째 인자로는, await할 액션을 넣는다. 이 제네레이터 함수는 GET_POSTS의 액션이 일어날때까지 기다린다. 이 액션이 실행된다면, 그때서야 다음 스텝으로 움직인다. 
+}
+
+export default function* postsSaga() {
+    yield all([getPostsWatcher()])
+}
--- redux-saga-tutorial-youtube-started-basic-tutorial/src/store/index.js
+++ redux-saga-tutorial-youtube-started-basic-tutorial/src/store/index.js
@@ -1,11 +1,14 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 import rootReducer from './reducer';
+import rootSaga from "./saga"
 
 const sagaMiddleware = createSagaMiddleware();
 
 const withDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, withDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga)
 
 export default store;
